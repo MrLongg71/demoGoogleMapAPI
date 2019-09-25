464a520485f58df0e93f6f6388b88f3d3a4c0eb8
MrLongg71 <41661101+MrLongg71@users.noreply.github.com>
2019-09-25 10:12:26 +0700

    demo google map api

---
+++ app/src/main/java/vn/mrlongg71/demogooglemap/MapsActivity.java
@@ -0,0 +1,38 @@
+package vn.mrlongg71.demogooglemap;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.mMap = googleMap;
+
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions()
+                .position(sydney)
+                .title("Marker in Sydney"));
+
+        //dùng moveCamera để duy chuyển đến vị trí marker được add
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+}
